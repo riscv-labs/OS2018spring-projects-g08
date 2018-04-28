5848c706bbb42a2b12917b68e9df2ccc5c342fe0
LuChengTHU <c-lu15@mails.tsinghua.edu.cn>
2018-04-28 03:14:08 -0700

    rm IRQ_IDE

--- ucore/src/kern-ucore/arch/riscv_64/libs/arch.h
+++ ucore/src/kern-ucore/arch/riscv_64/libs/arch.h
@@ -168,9 +168,6 @@
 #define IRQ_COP      12
 #define IRQ_HOST     13
 
-#define IRQ_IDE1                14
-#define IRQ_IDE2                15
-
 #define DEFAULT_RSTVEC     0x00001000
 #define CLINT_BASE         0x02000000
 #define CLINT_SIZE         0x000c0000
